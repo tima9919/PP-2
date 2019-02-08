3db5ceb4de623dd05914d1cb2b7c1565a8a5f2e6
Tima <t.kunapyanov@gmail.com>
2019-02-08 14:01:15 +0600

    lab2

---
+++ week2/task1/task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            string text = System.IO.File.ReadAllText(@"D:\text.txt");
+            for (int i = 0, j = text.Length - 1; i < text.Length / 2; i++, j--)
+            {
+                if (text[i] != text[j])
+                {
+                    check = true;
+                    break;
+
+                }
+                if (check == true)
+                {
+                    Console.WriteLine("No");
+
+                }
+                else
+                {
+                    Console.WriteLine("Yes");
+                }
+            }
+        }
+    }
+}
---
+++ week2/task4/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\path";
+            string path1 = @"D:\path1";
+            Directory.CreateDirectory(path);
+            Directory.CreateDirectory(path1);
+            string file = "test.txt";
+            File.Create(path + @"\" + file).Dispose();
+            string com = Console.ReadLine();
+            string sfile = path + @"\" + file;
+            string dfile = path1 + @"\" + file;
+            File.Move(sfile, dfile);
+        }
+    }
+}
