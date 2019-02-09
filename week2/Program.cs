24416d881a80bc7e2a9b05dc33b6468746828453
tima9919 <47049655+tima9919@users.noreply.github.com>
2019-02-09 13:37:27 +0600

    Add files via upload

---
+++ week2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task31
+{
+    class Program
+    {
+    public static void PrintSpaces(int level)
+    {
+        for (int i = 0; i < level; i++)
+            Console.Write("     ");
+    }
+
+    public static void Ex5(DirectoryInfo dir, int level)
+    {
+        foreach (FileInfo f in dir.GetFiles())
+        {
+            PrintSpaces(level);
+            Console.WriteLine(f.Name);
+        }
+        foreach (DirectoryInfo d in dir.GetDirectories())
+        {
+            PrintSpaces(level);
+            Console.WriteLine(d.Name);
+            Ex5(d, level + 1);
+        }
+    }
+
+    
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo("C:/Users/Tima/Desktop/pp2/PP-2");
+            Ex5(dir, 0);
+ Console.ReadLine();
+        }
+       
+    }
+}
